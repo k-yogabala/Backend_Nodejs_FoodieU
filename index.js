95906389c3004a5ffa3003800751fc7e86608cd3
k-yogabala <kalliyogabala2003@gmail.com>
2024-08-20 12:10:21 +0530

    added path module

--- index.js
+++ index.js
@@ -6,14 +6,17 @@ const vendorRoutes=require('./routes/vendorRoutes');
 const bodyParser=require('body-parser');
 const firmRoutes = require('./routes/firmRoutes');
 const productRoutes = require('./routes/productRoutes');
-const path=require('path');
+const cors = require('cors');
 
 const app = express()
+
 
 //defining route
 const PORT = process.env.PORT || 4000;
 
 dotEnv.config();
+app.use(cors())
+
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>console.log("MongoDB connected succesfully!"))
 .catch((error)=>console.log(error))
@@ -32,7 +35,6 @@ app.listen(PORT,()=>{
 
 
 // creating a route
-app.use('/',(req,res)=>
-{
-    res.send("<h1> Welcome to FoodieU");
+app.use('/', (req, res) => {
+    res.send("<h1> Welcome to SUBY");
 })
